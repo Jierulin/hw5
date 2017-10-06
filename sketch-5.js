9c8e4547fed7639ec45b3751c8554865b2b2af3d
Jierulin <31756333+Jierulin@users.noreply.github.com>
2017-10-06 15:54:34 +0800

    Create sketch-5.js

---
+++ sketch-5.js
@@ -0,0 +1,61 @@
+var circle = {
+  x: 100, 
+  y: 400,
+  vx: 0,
+  vy: -4,
+  r: 10
+};
+
+var circle2 = {
+  x: 160, 
+  y: 400,
+  vx: 0,
+  vy: -4,
+  r: 10
+};
+
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(255);
+  
+  paint(circle);
+  paint(circle2);
+  
+  move(circle);
+  move(circle2);
+  
+  reset(circle);
+  reset(circle2);
+}
+
+function paint(circle) {
+  ellipse(circle.x, circle.y, circle.r*2, circle.r*2);
+}
+
+function move(circle) {
+	circle.x += random(-10,10);
+  circle.x += circle.vx; // circle.x = circle.x + circle.vx
+  circle.y += circle.vy;
+}
+
+function reset(circle) {
+  if (circle.x > width || circle.x < 0) {
+    circle.vx = - circle.vx;
+	
+function keyPressed() {
+  if (keyCode === LEFT_ARROW) {
+    circle.x+=50;
+  } else if (keyCode === RIGHT_ARROW) {
+   circle.x+=50;
+  }
+}
+		
+  }
+  if (circle.y <0 ) {
+    
+		circle.y=400;
+  }
+}
