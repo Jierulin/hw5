5016a6775816388d41d616915983e1498aa522f3
Jierulin <31756333+Jierulin@users.noreply.github.com>
2017-10-08 02:43:29 -0700

    Create circles-2.js

---
+++ circles-2.js
@@ -0,0 +1,13 @@
+var sizes = [40, 70, 90, 30, 60,10,10,10];
+
+function setup() {
+  createCanvas(600, 600);
+	colorMode(HSB);
+  noStroke();
+    
+  for (var i = 0; i < sizes.length; i += 1) {
+    fill(15,100,100);
+    ellipse(50+i*80, height/2, sizes[i], sizes[i]);
+  }
+}
+
