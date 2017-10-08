ce4f4c8072f8513c8fd0d48529385137426e87c2
Jierulin <31756333+Jierulin@users.noreply.github.com>
2017-10-07 22:16:28 -0700

    Create lewitt-1180.js

---
+++ lewitt-1180.js
@@ -0,0 +1,13 @@
+//animated
+
+function setup(){
+
+	createCanvas(350,350)
+}
+
+function draw(){
+	background(0);	
+	stroke(255);
+	fill(255);
+	ellipse(random(350),random(350),random(4),random(4));
+}
