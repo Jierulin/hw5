c34e3ba1bc59b12688c9b1d67ff876a434a88fc0
Jierulin <31756333+Jierulin@users.noreply.github.com>
2017-10-08 04:03:24 -0700

    Create array-7.js

---
+++ array-7.js
@@ -0,0 +1,26 @@
+var shade=[10,20,25,45,60,70,80,90,100]
+var clicknumber=0
+
+function setup() {
+  createCanvas(400, 400);
+
+}
+
+
+function draw() {
+
+
+	colorMode(HSB);
+  noStroke();
+	
+	for(i=0;i<shade.length-1;i=i+1){
+  fill(shade[i], 100, 100);
+  rect(i*width/(shade.length+clicknumber), 0, width/(shade.length+clicknumber), height);
+	if (shade[shade.length+clicknumber]>8){
+		shade[shade.length-1] = random(360)	
+		fill(random(360),100,100);
+		 rect(i*width/(shade.length+clicknumber), 0, width/(shade.length+clicknumber), height);	}
+	}
+	
+	clicknumber+=1	
+}
